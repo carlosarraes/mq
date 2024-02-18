7a226e73316f8a176a1a05d1e1b5b185630f7382
carraes <carraeshb@gmail.com>
2024-02-18 01:05:00 -0300

    chore: setup db

---
+++ src/db/config.rs
@@ -0,0 +1,12 @@
+use sqlx::{ sqlite::SqlitePoolOptions, SqlitePool };
+
+pub async fn init_db() -> Result<SqlitePool, sqlx::Error> {
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+    println!("Connecting to SQLite");
+
+    let pool = SqlitePoolOptions::new()
+        .connect(&db_url)
+        .await?;
+
+    Ok(pool)
+}
